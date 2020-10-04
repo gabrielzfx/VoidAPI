d6c7cacbf3ba912a6462d07a430d0b628efa3c81
gabrielzfx <chmodxxzrxx@gmail.com>
2020-10-04 10:28:49 -0400

    add new routes and models

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const url = ''
+
+mongoose.connect(url, {useNewUrlParser:true})
+const connection = mongoose.connection
+
+connection.on('open', () => {
+    console.log('connected...')
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routes/generic')
+app.use('/generic',genericRouter)
+
+app.listen(9000, () => {
+    console.log('Server started')
+})
---
+++ src/routes/generic.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const Generic = require('../models/generic')
+
+
+router.get('/', async(req,res) => {
+    try{
+           const generic = await Generic.find()
+           res.json(generic)
+    }catch(err){
+        res.send('Error ' + err)
+    }
+})
+
+router.get('/:id', async(req,res) => {
+    try{
+           const generic = await Generic.findById(req.params.id)
+           res.json(generic)
+    }catch(err){
+        res.send('Error ' + err)
+    }
+})
+
+router.post('/', async(req,res) => {
+    const generic = new Generic({
+        name: req.body.name,
+        tech: req.body.tech,
+        sub: req.body.sub
+    })
+
+    try{
+        const a1 =  await generic.save() 
+        res.json(a1)
+    }catch(err){
+        res.send('Error')
+    }
+})
+
+router.patch('/:id',async(req,res)=> {
+    try{
+        const generic = await Generic.findById(req.params.id) 
+        generic.sub = req.body.sub
+        const a1 = await generic.save()
+        res.json(a1)   
+    }catch(err){
+        res.send('Error')
+    }
+
+})
+
+module.exports = router
