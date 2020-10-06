f3be468e82ebed8f16c647976f5e8a13ec316292
gabrielzfx <chmodxxzrxx@gmail.com>
2020-10-06 12:13:00 -0400

    server working, but with hardcode passwords

--- src/index.js
+++ src/index.js
@@ -2,20 +2,30 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-const url = ''
+// GET      generic
+// GET      generic/<id>
+// POST     generic/
+// PATH     generic/<id>
+// DELETE   generic/<id>
 
-mongoose.connect(url, {useNewUrlParser:true})
+// Connection
+
+const pass   = 'jEj0Hwbxt56yNy29'
+const dbname = 'voidapi'
+
+const url = `mongodb+srv://HackerMan:${pass}@cluster0.ywnwm.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true })
 const connection = mongoose.connection
 
+app.use(express.json())
 connection.on('open', () => {
     console.log('connected...')
 })
 
-app.use(express.json())
-
-const alienRouter = require('./routes/generic')
+const genericRouter = require('./routes/generic')
 app.use('/generic',genericRouter)
 
-app.listen(9000, () => {
+app.listen(3600, () => {
     console.log('Server started')
 })
